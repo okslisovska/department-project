e6d18bedfd64729615268392b3e39dc65c4dae9c
Oksana Lisovska <lisowska@i.ua>
2021-05-26 15:15:09 +0300

    add templates

--- app/api.py
+++ app/api.py
@@ -5,11 +5,11 @@ from app.models import Department, Employee
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 
-@bp.route('/employees/<int:id>')
-def get_employee(id):
-    return jsonify(Employee.query.get_or_404(id).to_dict())
-
-
 @bp.route('/departments')
 def get_departments():
     return jsonify([department.to_dict() for department in Department.query])
+
+
+@bp.route('/employees/<int:id>')
+def get_employee(id):
+    return jsonify(Employee.query.get_or_404(id).to_dict())
--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
 import requests
-from flask import Blueprint, request, url_for
+from flask import Blueprint, request, url_for, render_template
 
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
-def index():
-    return "<h1>Department-project</h1><p>will be here soon</p>"
-
-
 @bp.route('/departments')
 def departments():
     api_url = request.host_url[:-1] + url_for('api.get_departments')
     response = requests.get(api_url)
-    return response.text
+    departments = response.json()
+    return render_template('index.html', departments=departments)
+
+
+ENDPOINTS = ['departments', 'employees/<id>']
+
+@bp.route('/api')
+def api():
+    host_url = request.host_url
+    return render_template('api.html', host_url=host_url, endpoints=ENDPOINTS)
+
