a5ff733e3946035a1bf0234cad41cdcc001022f0
Oksana Lisovska <lisowska@i.ua>
2021-06-23 16:13:16 +0300

    discard resp_json function

--- app/main.py
+++ app/main.py
@@ -4,27 +4,19 @@ from flask import Blueprint, request, url_for, render_template, jsonify
 
 bp = Blueprint('main', __name__)
 
-ENDPOINTS = [('departments', 'GET'), ('employees', 'GET, POST'),\
-             ('employees/<id>', 'GET, PUT, DELETE'), ('search/<period>', 'GET')]
-
-
-def resp_json(endpoint, method='GET', data=None):
-    """
-    aggregate the full path to the specified api endpoint, send request, and return json data from response
-    """
-    api_url = request.host_url[:-1] + url_for(endpoint)
-    if method == 'GET':
-        resp = requests.get(api_url)
-    elif method == 'POST':
-        resp = requests.post(api_url, json=data)
-    return resp.json()
+ENDPOINTS = [('departments', 'GET'),
+             ('employees', 'GET, POST'),
+             ('employees/<id>', 'GET, PUT, DELETE'),
+             ('employees/search/<period>', 'GET')]
 
 
 @bp.route('/')
 @bp.route('/departments')
 def departments():
-    departments = resp_json('api.all_departments')
-    employees = resp_json('api.all_employees')
+    api_url_dep = request.host_url + url_for('api.all_departments')
+    api_url_emp = request.host_url + url_for('api.all_employees')
+    departments = requests.get(api_url_dep).json()
+    employees = requests.get(api_url_emp).json()
     for department in departments:
         salaries = [emp['salary'] for emp in employees if emp['department'] == department['name']]
         department['salary_avg'] = sum(salaries) // len(salaries)
@@ -33,7 +25,8 @@ def departments():
 
 @bp.route('/departments/<string:name>')
 def department(name):
-    resp = resp_json('api.all_employees')
+    api_url = request.host_url + url_for('api.all_employees')
+    resp = requests.get(api_url).json()
     employees = [employee for employee in resp if employee['department'] == name]
     return render_template('department.html', name=name, employees=employees)
 
@@ -48,7 +41,8 @@ def api():
 def create_employee():
     if request.method == 'POST':
         data = request.form
-        resp = resp_json('api.create_employee', method='POST', data=data)
+        api_url = request.host_url + url_for('api.create_employee')
+        resp = requests.post(api_url, json=data).json()
         return render_template('add.html', message=resp["message"])
     return render_template('add.html', message="")
 
@@ -58,7 +52,7 @@ def search_by_birthday():
     employees = []
     if request.method == 'POST':
         period = request.form['from'] + request.form['till']
-        api_url = request.host_url.rstrip('/') + \
+        api_url = request.host_url + \
                   url_for('api.search_by_birthday', period=period)
         employees = requests.get(api_url).json()
     return render_template('search.html', employees=employees)
